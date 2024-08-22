e73d7b33309a54659105cec5a2707284d90b1f83
Nick Falco <nfalco@nasuni.com>
2024-08-22 18:17:12 -0400

    Switch to using serverless google cloud run and stream verbose output to client

--- src/api.py
+++ src/api.py
@@ -12,7 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import Flask, jsonify, request
+import logging
+from multiprocessing import Process, Queue, Pipe
+from flask import Flask, request
+from flask import stream_with_context
 from flask_cors import CORS, cross_origin
 from eat.core.components import Groupoid, TermOperation
 from eat.beam_algorithm.beam import BeamEnumerationAlgorithm
@@ -20,13 +23,28 @@ app = Flask(__name__)
 cors = CORS(app)
 
 
-def run_beam_algorithm(groupoid, target):
+class PipeHandler(logging.Handler):
+
+    def __init__(self, conn):
+        super().__init__()
+        self.conn = conn
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.conn.send(log_entry)
+
+
+def run_beam_algorithm_subprocess(groupoid, target, conn, queue):
+    # capture the log output from the beam algorithm
+    pipe_handler = PipeHandler(conn)
+
     algorithm = "MFBA"
     mtgm = "random-term-generation"
     prob = 0.1
     beam_width = sub_beam_width = 2
     grp = Groupoid(groupoid)
-    to = TermOperation(groupoid=grp, term_variables=["x", "y", "z"], target=target)
+    to = TermOperation(groupoid=grp, term_variables=["x", "y", "z"],
+                       target=target)
     beam = BeamEnumerationAlgorithm(
         grp,
         to,
@@ -34,9 +52,31 @@ def run_beam_algorithm(groupoid, target):
         male_term_generation_method=mtgm,
         term_expansion_probability=prob,
         beam_width=beam_width,
-        sub_beam_width=sub_beam_width)
+        sub_beam_width=sub_beam_width,
+        logging_handler=pipe_handler)
     node, search_time = beam.run(verbose=True)
-    return node.term, search_time
+    queue.put((node.term, search_time))
+    conn.close()
+
+
+def run_beam_algorithm(groupoid, target):
+    queue = Queue()
+    parent_conn, child_conn = Pipe()
+    p = Process(target=run_beam_algorithm_subprocess,
+                args=(groupoid, target, child_conn, queue))
+    p.start()
+
+    while True:
+        if parent_conn.poll():
+            log_message = parent_conn.recv()
+            print(log_message)
+            yield log_message + '\n'
+        if not p.is_alive():
+            break
+    p.join()
+    term, search_time = queue.get()
+    yield term + '\n'
+    yield f"{search_time:.2f}" + '\n'
 
 
 @app.route('/', methods=['GET'])
@@ -50,9 +90,13 @@ def landing():
 def eat():
     data = request.get_json()  # Get the JSON data
     groupoid = [int(t) for t in data['groupoid']]
-    target = [[t] for t in data['target']]
-    term, search_time= run_beam_algorithm(groupoid, target)
-    response = jsonify({'term': term, 'search_time': f"{search_time:.2f}"})
+    target = [[int(t)] for t in data['target']]
+
+    # Use stream_with_context to stream the output
+    response = app.response_class(
+        stream_with_context(run_beam_algorithm(groupoid, target)),
+        mimetype='text/plain'
+    )
     return response
 
 
